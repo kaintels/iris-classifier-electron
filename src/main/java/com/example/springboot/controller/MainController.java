c01482317dded54de026a2a970bde6a9eb865c31
Seungwoo Han <seungwoohan0108@gmail.com>
2023-02-18 00:06:36 +0900

    Update MainController

--- src/main/java/com/example/springboot/controller/MainController.java
+++ src/main/java/com/example/springboot/controller/MainController.java
@@ -1,24 +1,27 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.dto.ProjectDTO;
 import com.example.springboot.dto.FormDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
+
 
 @Controller
 public class MainController {
-
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @PostMapping("predict")
+    @PostMapping("/predict")
     public String hello(FormDTO formDVO, Model model) {
         model.addAttribute("data", formDVO.getA());
         System.out.println(formDVO.getA());
         return "predict";
     }
 }
+
