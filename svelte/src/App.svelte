b223ca54b97bb41d62c83c858bee46b75f39626e
Seungwoo Han <seungwoohan0108@gmail.com>
2023-02-18 00:04:25 +0900

    Add App

---
+++ svelte/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let project;
+	let date
+  
+	fetch("http://localhost:8080/project_info").then((response) => {
+	  response.json().then((res) => {
+		project = res.project;
+		date = res.date;
+	  });
+	});
+  </script>
+  
+  <h1>{project}</h1>
+  <h2>{date}</h2>
