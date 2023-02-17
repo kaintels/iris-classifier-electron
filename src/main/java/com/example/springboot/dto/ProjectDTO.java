7cede75c8c0fa801d5326af3a1e62f44c9f7bb55
Seungwoo Han <seungwoohan0108@gmail.com>
2023-02-18 00:06:11 +0900

    Add ProjectDTO

---
+++ src/main/java/com/example/springboot/dto/ProjectDTO.java
@@ -0,0 +1,24 @@
+package com.example.springboot.dto;
+
+import java.util.Date;
+
+public class ProjectDTO {
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String project;
+    public Date date;
+}
