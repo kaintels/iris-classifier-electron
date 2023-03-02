3aa9b564ca47882b7aa3418b17776d3374ecec4b
Seungwoo Han <seungwoohan0108@gmail.com>
2023-03-02 16:29:48 +0900

    Update electron.js

--- public/electron.js
+++ public/electron.js
@@ -33,7 +33,7 @@ function createWindow() {
 /** 초기화가 끝나게 되면 실행 */
 app.on('ready', () => {
     // 메인 창 생성
-    const result = require('child_process').spawn('java', [ '-jar' , "./build/libs/spring-boot-0.0.1-SNAPSHOT.jar"]);
+    const result = require('child_process').spawn('java', [ '-jar' , "./resources/iris-classifier-electron-0.0.1-SNAPSHOT.jar"]);
 // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
 
 
